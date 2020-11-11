c4c0fe880086d0a73644cc02b443d402ee072e58
ninesky00 <jiangzho@gmail.com>
2020-11-11 10:27:22 -0600

    Add market setups

--- test/market_test.rb
+++ test/market_test.rb
@@ -7,11 +7,29 @@ require './lib/market'
 class MarketTest < MiniTest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
   end
 
   def test_attributes_are_instantiated
     assert_instance_of Market, @market
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal [], @market.vendors
+  end
+
+  def test_
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(item4, 50)
+    @vendor2.stock(item3, 25)
+    @vendor3.stock(item1, 65)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(vendor3)
   end
 end
