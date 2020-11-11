c6b6f0c92c0eafdfd157e6210c63dbb6d3d57963
ninesky00 <jiangzho@gmail.com>
2020-11-11 10:06:06 -0600

    Add initial test for item class

---
+++ test/item_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+
+class ItemTest < MiniTest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_attributes_are_instantiated
+    assert_instance_of Item, @item1
+    assert_instance_of Item, @item2
+    assert_equal "Tomato", @item2.name
+    assert_equal 0.5, @item.price
+  end
+end
