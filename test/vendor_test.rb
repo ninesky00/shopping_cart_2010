ece4785b826247e70c4f45cc3fd46e5cd66db469
ninesky00 <jiangzho@gmail.com>
2020-11-11 10:15:18 -0600

    Add check_stock and stock method tests

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,16 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require './lib/item'
 
 class VendorTest < MiniTest::Test
   def setup
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   def test_attributes_are_instantiated
     assert_instance_of Vendor, @vendor
     assert_equal "Rocky Mountain Fresh", @vendor.name
     assert_equal ({}), @vendor.inventory
+  end
+
+  def test_can_stock_inventory
+    assert_equal 0, @vendor.check_stock(@item1)
+    @vendor.stock(@item1, 30)
+    assert_equal ({@item1 => 30}), @vendor.inventory
   end
 
 end
